d3af07ac2b94ba2bfebdd5fc0123953ab501f7a3
ayerzafelix <ayerzaf@udesa.edu.ar>
2023-10-09 11:28:09 -0300

    primer push

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {auth} from ".src/firebase/config";
+import { registerRootComponent } from 'expo';
+
 
 export default function App() {
+
+  function register(email, pass){
+    auth.createUserWithEmailAndPassword(email, pass)
+     .then( response => {
+         console.log(response);
+      })     
+     .catch( error => {
+       console.log(error);
+     })
+  }
+
+  function login(email, pass){
+    auth.signInWithEmailAndPassword(email, pass)
+     .then((response) => {
+         console.log(response);
+     })
+     .catch(error => {
+       console.log(error);
+     })
+  }
+ 
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwQrv90798xtCfsi5DcxJ5bgL-FVwwn7o",
+  authDomain: "progra3firebase.firebaseapp.com",
+  projectId: "progra3firebase",
+  storageBucket: "progra3firebase.appspot.com",
+  messagingSenderId: "456249522391",
+  appId: "1:456249522391:web:cfe415811da72eda6e9892"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
