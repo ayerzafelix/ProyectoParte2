c43ef9f21b2f6e34903d3aeb57029fc50f22b506
joaquin-huergo <jhuergo@udesa.edu.ar>
2023-11-15 00:11:30 -0300

    arreglo en app

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MenuBar from './src/components/MenuBar/MenuBar';
 
 export default function App() {
   return (
-    <MenuBar />
-  );
+  <MenuBar />)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
