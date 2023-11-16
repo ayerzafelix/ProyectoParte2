dbede5bf6354045b064bb13dd0631322929fe307
ayerzafelix <ayerzaf@udesa.edu.ar>
2023-11-16 12:11:26 -0300

    comentarios falta render y más

--- src/screens/Comentario/Comentario.js
+++ src/screens/Comentario/Comentario.js
@@ -1,4 +1,34 @@
+import React, { Component } from 'react';
+import {TextInput,TouchableOpacity,View,Text,StyleSheet,FlatList,} from 'react-native';
+import { db, auth } from '../../firebase/config';
 
 
+  
+  
+  class Comentario extends Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        elComentario:'',
+        usuario:'',
+        data:{}
+      }
+    }
+  
+    componentDidMount(){
+      db
+      .collection('post')
+      .doc(this.props.route.params.usuario)
+      .onSnapshot(doc => {
+        this.setState({
+          usuario: doc.usuario,
+          data: doc.data(),
+        })
+      })
+    }
+  
 
-
+  }
+  
+  
+  export default Comentario
