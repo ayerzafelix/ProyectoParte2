7dd9693412caef1ab3e89de29bb9620482aeb99c
JanoMeja <jmejalelaty@udesa.edu.ar>
2023-11-16 01:53:31 -0300

    perfil

--- src/screens/Comentario/Comentario.js
+++ src/screens/Comentario/Comentario.js
@@ -1,35 +1,3 @@
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  // Estilos para MyCamera
-  
-  // Estilos para AddPost
-  
-  // Estilos para Home
-  textHome: {
-    fontFamily: 'Arial, sans-serif',
-    color: '#2c3e50',
-    fontWeight: 'bold',
-    fontSize: 35,
-    textAlign: 'center',
-    backgroundColor: '#ecf0f1',
-    marginBottom: 20,
-    marginTop: 20,
-  },
-
-  // Estilos para MiPerfil
-  
-  // Estilos compartidos
-  text: {
-    fontFamily: 'Arial, sans-serif',
-    color: '#2c3e50',
-    fontSize: 20,
-    flexDirection: 'column',
-  },
-});
-
-export default styles;
-
 
 
 
--- src/screens/MyProfile/MyProfile.js
+++ src/screens/MyProfile/MyProfile.js
@@ -1,119 +1,190 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View, TouchableOpacity, FlatList} from 'react-native'
-import { auth, db } from '../../firebase/config'
-import Post from '../../components/Post/Post'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, Alert } from 'react-native';
+import { auth, db, storage } from '../../firebase/config';
+import Post from '../../components/Post/Post';
 
- class MyProfile extends Component {
-    constructor(props) {
-        super(props);
-            this.state = {
-            posts: [],
-            user: []  
-            }
-    }
-    componentDidMount(){
-        if (auth.currentUser) {
-            /*db.collection('posts')
-            .where('owner','==', auth.currentUser.email)
-            .onSnapshot((docs) => {
-                let posteos = []
-                docs.forEach((doc) => {
-                    posteos.push({
-                        id: doc.id,
-                        data: doc.data()
-                    })
-                })
-                this.setState({
-                    posts: posteos,
-                })
-            })*/
-            db.collection('users')
-            .where('owner','==', auth.currentUser.email)
-            .onSnapshot((docs) => {
-                let usuario = []
-                docs.forEach((doc) => {
-                    usuario.push({
-                        id: doc.id,
-                        data: doc.data()
-                    })
-                })
-                this.setState({
-                    user: usuario[0].data
-                })
-            })   
-          }
+class MyProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUserData();
+    this.fetchUserPosts();
+  }
+
+  fetchUserData() {
+    const currentUser = auth.currentUser;
+
+    currentUser & db.collection('users')
+    .where('owner', '==', currentUser.email)
+    .onSnapshot((docs) => {
+
+        let user = []
+        docs.forEach(doc => {
+            user.push({
+                id: doc.id,
+                data: doc.data()
+            })
+        })
+        this.setState({ user: user[0] });
+    });
+  }
+
+  fetchUserPosts() {
+    const currentUser = auth.currentUser;
+
+    db.collection('posts')
+      .where('owner', '==', currentUser.email)
+      .onSnapshot((querySnapshot) => {
+        let posts = [];
+        querySnapshot.forEach((doc) => {
+          posts.push({
+            id: doc.id,
+            datos: doc.data(),
+          });
+        });
+        this.setState({
+          posts: posts,
+        });
+      });
+  }
+
+  deletePost(postId) {
+    db.collection('posts')
+      .doc(postId)
+      .delete()
+      .then(() => {
+        console.log("Post successfully deleted!");
+        // Actualizar la lista de posts después de borrar uno
+        this.fetchUserPosts();
+      })
+      .catch((error) => {
+        console.error("Error deleting post: ", error);
+      });
+  }
+
+  signOut() {
+    auth.signOut()
+      .then(() => {
+        this.props.navigation.navigate('Login');
+        this.forceUpdate();
+      })
+      .catch((error) => {
+        console.error("Error during logout:", error);
+      });
+  }
+
+  render() {
+    const { user, posts } = this.state;
+
+    if (!user) {
+      return (
+        <View style={styles.loadingContainer}>
+          <Text>Loading...</Text>
+        </View>
+      );
     }
 
-    signOut() {
-        auth.signOut()
-        this.props.navigation.navigate('Login')
-      }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textUsername}>Username: {user.data.name}</Text>
+        <Text style={styles.textEmail}>Email: {user.data.owner}</Text>
+        <Text style={styles.textMiniBio}>Mini Bio: {user.data.biografia || 'No mini bio available'}</Text>
 
-    render() {
-        console.log(auth.currentUser)
-        console.log(this.state.user)
-        return (
-          
-            <View>
-                <Text style={styles.infoLogin}>TU PERFIL</Text>
-                <Text style={styles.infoLogin1}>{this.state.user.name}</Text>
-                <Text style={styles.infoLogin}>Biografia</Text>
-                <Text style={styles.infoLogin}>{this.state.user.biografia}</Text>
-                <Text style={styles.infoLogin}>Email: {this.state.user.owner}</Text>
-                <Text style={styles.infoLogin}>Cantidad de posteos: {this.state.posts.length}</Text>
-                <FlatList
-                    data={this.state.posts}
-                    keyExtractor={(post) => post.id}
-                    renderItem={({item}) => <Post postData={item}/>}
-                />
-                 <TouchableOpacity style={styles.contain}onPress={()=>this.signOut()}>
-                    <Text style={styles.button}>CERRAR SESION</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+        {user.profileImageUrl && (
+          <Image source={{ uri: user.profileImageUrl }} style={styles.profileImage} />
+        )}
+
+        <Text style={styles.textTotalPosts}>Total Posts: {posts.length}</Text>
+
+        <FlatList
+        style={styles.posts}
+        data={posts}
+        keyExtractor={(post) => post.id.toString()}
+        renderItem={({ item }) => {return <Post infoPost={item} navigation={this.props.navigation} />;}}
+        />
+
+        <TouchableOpacity style={styles.logoutButton} onPress={() => this.signOut()}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-
-    contain:{
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-
-    button: {
-        backgroundColor: "rgb(245, 32, 32)",
-        paddingHorizontal: 10,
-        paddingVertical: 6,
-        fontSize: 15,
-        textAlign: "center",
-        borderRadius: 6,
-        borderWidth: 1,
-        borderStyle: "solid",
-        borderColor: "rgb(0,0,0)",
-        width: '30%',
-        marginBottom: 15,
-        alignSelf: 'center',
-      },
-
-      infoLogin:{
-        fontSize: 16,
-        fontWeight: `bold`,
-        textAlign: `center`,
-        marginBottom: 10,
-        marginTop: 10,
-      },
-
-      infoLogin1:{
-        fontSize: 29,
-        fontWeight: `bold`,
-        textAlign: `center`,
-        marginBottom: 10,
-        marginTop: 10,
-        color: "#E5E24A",
-        fontFamily: 'Montserrat'
-      },
-     
-})
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginVertical: 16,
+  },
+  postContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  deleteButton: {
+    color: 'red',
+    marginTop: 8,
+  },
+  logoutButton: {
+    backgroundColor: 'black',
+    padding: 12,
+    borderRadius: 8,
+    marginTop: 16,
+    alignItems: 'center',
+  },
+  textUsername: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  textEmail: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  textMiniBio: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  textTotalPosts: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  textPost: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  textLoading: {
+    fontSize: 18,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default MyProfile;
